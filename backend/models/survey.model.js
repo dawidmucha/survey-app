9d39693703be14e17d5036461249806346f25749
Dawid Mucha <dawidmucha98@gmail.com>
2020-02-06 10:37:23 +0100

    db connection not working

--- backend/models/survey.model.js
+++ backend/models/survey.model.js
@@ -3,31 +3,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const surveySchema = new Schema({
-  demographics: { type: Object, required: true, properties: {
-		age: { type: Number, required: true},
-		gender: { type: String, required: true},
-		ethnicity: { type: String, required: true},
-		education: { type: String, required: true},
-		employment: { type: String, required: true},
-		country: { type: String, required: true},
-	}},
-	wouldYouRather: { type: Object, required: true, properties: {
-		pudding: { type: Number, required: true},
-		chocolate: { type: String, required: true},
-		dinner: { type: String, required: true},
-		direction: { type: String, required: true},
-		employment: { type: String, required: true},
-		language: { type: String, required: true},
-	}},
-	howdYouSpend: {type: String, required: true },
-	dontStopMeNow: { type: Object, required: true, properties: {
-		floating: { type: Number, required: true },
-		tiger: { type: Number, required: true },
-		racing: { type: Number, required: true },
-		rocketship: { type: Number, required: true },
-		explode: { type: Number, required: true },
-		travelling: { type: Number, required: true },
-	}}
+  age: { type: String, required: true},
+  gender: { type: String, required: true},
+  education: { type: String, required: true},
+  q1: { type: String, required: true},
+  q3: { type: String, required: true},
+  hemisphere: { type: String, required: true},
+  q4: { type: String, required: true},
+  chocolate: { type: String, required: true},
+  ethnicity: { type: String, required: true},
+  employment: { type: String, required: true},
+  english: { type: String, required: true},
+  q5: { type: String, required: true},
+  value: { type: String, required: true},
+  q6: { type: String, required: true},
+  staple: { type: String, required: true},
+  pudding: { type: String, required: true},
+  q2: { type: String, required: true},
+  framework: { type: String, required: true}
 }, {
   timestamps: true,
 });
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const router = require('express').Router()
 const cors = require('cors')
 const mongoose = require('mongoose')
+let Survey = require('./models/survey.model')
 
 require('dotenv').config()
 
@@ -19,8 +21,22 @@ connection.once('open', () => {
 	console.log('MongoDB database connectrion extablished successfully')
 })
 
-app.use('/', (req, res) => {
-	res.send('woo')
+router.route('/survey4').post((req, res) => {
+	const results = req.body.results
+	
+	const newSurvey = new Survey({
+		results
+	})
+
+	newSurvey.save()
+  .then(() => res.json('Survey added!'))
+  .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/results').get((req, res) => {
+	// Survey.find().then(results => {
+	// 	return res.json(results)
+	// }).catch(err => res.status(400).json('Error: ', err))
 })
 
 app.listen(port, () => {
