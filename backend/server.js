60002a74a3db88ff270efefcc1d9fb3893aed087
Dawid Mucha <dawidmucha98@gmail.com>
2020-02-22 12:43:02 +0100

    path.resolve change

--- backend/server.js
+++ backend/server.js
@@ -41,10 +41,10 @@ const surveyRouter = require('./routers/survey')
 app.use('/', surveyRouter)
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../build'))
+  app.use(express.static('/../build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '/../build/index.html'))
   })
 }
 
